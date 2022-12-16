2d859bc12546cdf1e6c1ef98ebe45eb52ebcd1b8
gaaats <mr.apemi@gmail.com>
2022-12-16 18:29:36 +0200

    епепепе

--- app/src/main/java/com/mobirix/basketballkin/FakeGameFragment.kt
+++ app/src/main/java/com/mobirix/basketballkin/FakeGameFragment.kt
@@ -5,55 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.mobirix.basketballkin.databinding.FragmentFakeGameBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FakeGameFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FakeGameFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fake_game, container, false)
+    ): View {
+        bbgbgbg = FragmentFakeGameBinding.inflate(inflater, container, false)
+        return nnhnhnh.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FakeGameFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FakeGameFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        try {
+            nnhnhnh.btnPlayGameeee.setOnClickListener {
+                try {
+                    findNavController().navigate(R.id.action_fakeGameFragment_to_itIsGameFragment)
+                } catch (e: Exception) {
+                    Snackbar.make(
+                        nnhnhnh.root,
+                        "Some error, restarting...",
+                        Snackbar.LENGTH_LONG
+                    )
+                        .show()
                 }
             }
+
+        } catch (e: Exception) {
+            Snackbar.make(nnhnhnh.root, "Some error, reload...", Snackbar.LENGTH_LONG)
+                .show()
+        }
+        super.onViewCreated(view, savedInstanceState)
+
     }
+
+    override fun onDestroyView() {
+        bbgbgbg = null
+        super.onDestroyView()
+    }
+
+    private var bbgbgbg: FragmentFakeGameBinding? = null
+    private val nnhnhnh
+        get() = bbgbgbg ?: throw RuntimeException("FragmentFakeGameBinding = null")
+
+
 }
